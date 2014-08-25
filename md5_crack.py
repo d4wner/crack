0255af72515d72e6b5c50c73eae81028f88b83d5
d4wner <d4wner@gmail.com>
2014-08-25 23:19:00 +0800

    08/25

--- md5_crack.py
+++ md5_crack.py
@@ -1,4 +1,4 @@
-# -*- coding=gbk -*-
+# -*- coding=utf-8 -*-
 import sys
 import socket
 import urllib
@@ -6,85 +6,99 @@ import re
 import string
 import urllib2
 import threading
+import HTMLParser
 
 def showInfo():
     print """
-     命令格式：MD5Crack.py -hash md5hash
+     命令格式：md5_crack.py -hash md5hash \n
+			or md5_crack.py -dic
            """
-def MD5Crack(Hash):
-    print "trying md5cracker.org    ====>"
-    MD5Crack_md5cracker(Hash)
-    print "trying www.md5.asia    ====>"
-    MD5Crack_md5asia(Hash)
-    print "trying www.xmd5.com    ====>"
-    MD5Crack_xmd5(Hash)
-   
+
+    #print "trying www.cmd5.com    ====>"
+    #try:
+    #	if crack_md5(HASH):
+    #		return True
+    #except:
+    #	pass
+
+
 #From http://md5cracker.org/
-list_of_result=[]   #存放结果
-thread_pool=[]   
-def MD5Crack_md5cracker(Hash):
-    if len(Hash)!=32:   #这个网站只支持32位的
-        print "Not Found"
-        return
-    str_url=["http://md5cracker.org/hash.php?hash=","&id=","0"]
-    str_url.insert(1,Hash)
+#list_of_result=[]   #存放结果
+#thread_pool=[]   
+comcn_tmp=[]
 
-    global g_mutex
-    g_mutex=threading.Lock()    #初始化互斥量
+global f,s
+
+class timer(threading.Thread): #The timer class is derived from the class threading.Thread  
+    def __init__(self, HASH):  
+        threading.Thread.__init__(self)  
+        self.HASH = HASH    
    
-    nrange=range(1,14)
-    for i in nrange:
-        str_url[3]=str(i)
-        url="".join(str_url)
-        #print url
-
-        #开线程
-        th=threading.Thread(target=md5crackerCommon,args=(i ,url))
-        thread_pool.append(th)
-        th.start()
-
-    #阻塞主线程。collect all threads
-    pos=1
-    for pos in nrange:
-        threading.Thread.join(thread_pool[pos-1])
-
-    if len([result for result in list_of_result if result!="no"]):   #存在不是“no”的情况，说明找到
-        print "Password Found:",[result for result in list_of_result if result!="no"][0]
-        exit(1)
-    else:
-        print "Not Found"
-   
-
-def md5crackerCommon(t_id,url):   
-    passPattern = re.compile(r'#--#([\d|\D]*)#--#')
-    conststr="-www-md5cracker-org"
-   
-    try:
-        sock=urllib.urlopen(url)
-        htmlSources=sock.read()
-    except:
-        return
-    else:       #如果没有urlopen成功，sock是不存在的。这里用个else正好!
-        sock.close()
-
-    result=passPattern.search(htmlSources).groups()[0]
-    if len(result):         #返回结果为0，则代表没有超时
-        if string.find(result,conststr)==-1:    #返回-1，代表不存在，即正确
+    def run(self):  
+        try:
+            if crack_md5asia(self.Hash):
+                    return True
+        except:
             pass
-        else:
-            result="no"
-    else:
-        result="no"
+        
+        try:
+            if crack_cc(self.Hash):
+                return True
+        except:
+            pass
+        
+        try:
+            if crack_silic(self.HASH):
+                return True
+        except:
+            pass
+        
+        try:
+            if crack_comcn(self.HASH):
+                return True
+        except:
+            pass
 
-    g_mutex.acquire()
-    ######################受互斥量保护区代码##################################
-    list_of_result.append(result)
-    ########################################################################
-    g_mutex.release()
+        try:
+            if crack_somd5(self.HASH):
+                return True
+        except:
+            pass
+        print "[x]HASH Crack: "+HASH+" failed."
+        return False 
+              
+
+
+class Parselinks(HTMLParser.HTMLParser):
+    def handle_starttag(self,tag,attrs):
+        if tag == 'input':
+            for name,value in attrs:
+                if name == 'name':
+                    if value != 'sand':
+                        continue
+                    else:
+                        count=0
+                        for name,value in attrs:
+                            count=count+1
+                            if count == 3: 
+                                comcn_tmp.append(value)
+
+        if tag == 'input':
+            for name,value in attrs:
+                if name == 'name':
+                    if value != 'token':
+                        continue
+                    else:
+                        count=0
+                        for name,value in attrs:
+                            count=count+1
+                            if count == 3: 
+                                comcn_tmp.append(value)
+
 
 #From http://www.md5.asia/
-def MD5Crack_md5asia(Hash):   
-    str_url=["http://www.md5.asia/md5_decode.php?decoder=1&timeout=10&hash=",Hash]
+def crack_asia(Hash):   
+    str_url=["http://115.29.243.4/md5_decode.php?decoder=1&timeout=10&hash=",Hash]
     url="".join(str_url)
     #print url
             
@@ -92,59 +106,110 @@ def MD5Crack_md5asia(Hash):
         sock=urllib.urlopen(url)
         htmlSources=sock.read()
     except:
-        print "Not Found"
-        return
+        #print "Not Found"
+        return False
     else:
         sock.close()
     #本来这个地方，应该比较"未找到,"，但是由于编码的问题，会出问题。因此，改为16进制了。
     if string.find(htmlSources,"\346\234\252\346")!=-1:
-        print "Not Found"
+        return False
+        #print "Not Found"
     else:
-        print "Password Found:",htmlSources
-        exit(1)
+        #print "Password Found:",htmlSources
+        f.writelines(HASH+' '+resp+'\n')
+        return True
+        #exit(1)
 
-#From www.xmd5.com
-#由于需要使用Referer，需要使用urllib2。如果values不需要，则以None代替
-def MD5Crack_xmd5(Hash):   
-    str_url=["http://www.xmd5.com/md5/search.asp?hash=",Hash]
-    url="".join(str_url)
-    #print url
-   
-    #Referer: http://www.xmd5.com/md5/getpass.asp?info=admin
-    #对于XMD5这个网站，必须要这一项，否则无法正常查询。不管hash是什么，都是这个固定的值！
-    headers = { 'Referer' : 'http://www.xmd5.com/md5/getpass.asp?info=admin' }      
-    req = urllib2.Request(url,None,headers)
-    try:
-        response = urllib2.urlopen(req)
-        htmlSources = response.read()
-    except:
-        print "Not Found"
-        return
-    else:
-        response.close()
 
-    #使用正则表达式得到结果
-    passPattern = re.compile(r'size="3">([\d|\D]*)&nbsp;&nbsp;>>>Good Luck !<<<')
-    dic=passPattern.search(htmlSources)     #返回可能是空的，即表示没有破解成功
-    if dic is None:
-        print "Not Found"
-    else:
-        result=dic.groups()[0]  #查询结果
-        print "Password Found:",result
-        exit(1)
+def crack_comcn(HASH):
 
-#    print htmlSources
+    cj = cookielib.CookieJar();
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+    parsed = Parselinks()
+    parsed.feed(urllib.urlopen('http://www.md5.com.cn').read())
+    post_data = {'md': HASH ,'sand':comcn_tmp[0],'token':comcn_tmp[1],'submit':'MD5+Crack'}
+    post_data_urlencode = urllib.urlencode(post_data)
+    requrl = "http://www.md5.com.cn/md5reverse"
+    req = urllib2.Request(url = requrl,data =post_data_urlencode)
+    req.add_header('Referer', "http://www.md5.com.cn/")
+    resps = urllib2.urlopen(req)
+    match = re.findall('Result:.*green">.*<\/span',resps.read())
+    re_match=re.findall('green">.*<\/span><div',match[0])[0][7:-11]
+    #print re_match[0][7:-11]
+    f.writelines(HASH+' '+re_match+'\n')
+    return True
+
+
+
+def crack_silic(HASH):
+    post_data = {'isajax':'1' ,'md5':HASH}
+    post_data_urlencode = urllib.urlencode(post_data)
+    requrl = "http://cracker.blackbap.org/?do=search&language=en"
+    req = urllib2.Request(url = requrl,data =post_data_urlencode)
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    #print res
+    resp = re.findall('Password <strong>.*<\/strong>',res)[0][17:-9]
+    #print resp
+    f.writelines(HASH+' '+resp+'\n')
+    return True
+
+def crack_cc(HASH):
+    url='http://www.md5.cc/ShowMD5Info.asp?GetType=ShowInfo&no-cache=0.4669540437658686&md5_str='+HASH+'&_='
+    request = urllib2.Request(url)
+    request.add_header('Referer', "http://www.md5.cc/")
+    res=urllib2.urlopen(request).read()
+    resp = re.findall('25px">.*<\/span>',res)[0][6:-7].strip()
+    #print resp[6:-7].strip()
+    f.writelines(HASH+' '+resp+'\n')
+    return True
+
+def crack_somd5(HASH):
+
+    resp=urllib.urlopen('http://www.somd5.com/somd5-md5-js.html').read()
+    ajax_data=re.findall('isajax=.*&',resp)[0][7:-1]
+    post_data = {'isajax':ajax_data,'md5':HASH}
+    post_data_urlencode = urllib.urlencode(post_data)
+    requrl = "http://www.somd5.com/somd5-index-md5.html"
+    req = urllib2.Request(url = requrl,data =post_data_urlencode)
+    resps = urllib2.urlopen(req).read()
+    match = re.findall('<h1.*line;">.*<\/h1',resps)[0]
+    re_match = re.findall('">.*</',match)[0][2:-2]
+    f.writelines(HASH+' '+resp+'\n')
+    return True
+
 
 if '__main__' == __name__:         
-    if len(sys.argv)!=3 :
+
+    print """
+     =~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=
+     +-----------DemonSpider-----------+
+     +--------Md5-Cracker--V1.0--------+
+     +----------www.dawner.info--------+
+     +--md5_crack.py for single or dic-+
+     ===================================
+                    = =
+                   == ==
+                ===<-|->===
+                 ====D====
+                  ===e===
+                   ==m==
+                    =o=
+                     n
+    """
+
+
+    if len(sys.argv)<2 :
          print "参数错误"
          showInfo()
          exit(1)
      
-    cmds = ['-hash']
+    cmds = ['-hash','-dic']
      
     cmd = sys.argv[1]
-    Hash=sys.argv[2]
+    if len(sys.argv)!=3 :
+        Hash=sys.argv[2]
          
     if 0 == cmds.count(cmd):  
         print cmd
@@ -155,4 +220,32 @@ if '__main__' == __name__:
         #print 'Start working,Please waiting...'
         if cmd == '-hash':
             if len(Hash)==16 or len(Hash)==32:
-                MD5Crack(Hash)                               
+                crack_thread = timer(line.strip())
+                crack_thread.start()
+            else:
+                print "Hash长度出错."
+        elif cmd == 'dic':
+            s=open('success_result.txt','w+')
+            f=open('fail_result.txt','w+')
+            for line in open('hash.txt','r'):
+                if len(line.strip())==16 or len(line.strip())==32:
+                    crack_thread = timer(line.strip())
+                    crack_thread.start()
+                    #crack(line.strip())
+                else:
+                    continue
+            f.close()
+            s.close()
+        print "Crack ending...."
+
+
+        
+            #f.writelines()
+
+
+
+
+
+
+
+
