f1290a5dedbfa267eaff6a98be23a51d9038bc38
d4wner <d4wner@gmail.com>
2016-06-14 13:34:22 +0800

    add threads

--- ip_get.py
+++ ip_get.py
@@ -7,16 +7,16 @@ import socket
 import requests
 import socket
 import re
+import threading
 from bs4 import BeautifulSoup
 socket.setdefaulttimeout(2)
 import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
-timeout=5 # in seconds 
+timeout=3 # in seconds 
 socket.setdefaulttimeout(timeout)
 
 global headers,ipfile
-
 
 headers = {
     "User-Agent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)"
@@ -33,7 +33,9 @@ def xici_proxy():
         protocol = item[5].text
         #print ip,port,protocol
         proxyConfig = ip+':'+str(port)
-        proxy_test(proxyConfig,protocol,ip,port)
+        t = proxy_test(proxyConfig,protocol,ip,port)
+        t.start()
+        t.join()
 
 def _66ip_proxy():
     resp = requests.get('http://www.66ip.cn/mo.php?sxb=&tqsl=50&port=&export=&ktip=&sxa=&textarea=',headers = headers).content
@@ -42,12 +44,10 @@ def _66ip_proxy():
         ip = proxyConfig.split(':')[0]
         port = proxyConfig.split(':')[1]
         for protocol in ['https','http']:
-            proxy_test(proxyConfig,protocol,ip,port)
+            t = proxy_test(proxyConfig,protocol,ip,port)
+            t.start()
+            t.join()
 
-    #soup = BeautifulSoup(resp)
-    #trs = soup.find_all('tr',attrs = {'class':'container'}
-    #for tr in trs:
-    #    td = tr.find_all('td')
 
 def cz88_proxy():
     range_items = ["index.shtml"]+ ['http_%s.shtml' % n for n in range(2, 11)]
@@ -60,7 +60,9 @@ def cz88_proxy():
             ip = li.find('div',attrs = {'class':'ip'}).text
             port = li.find('div',attrs = {'class':'port'}).text
             proxyConfig = ip+":"+port
-            proxy_test(proxyConfig,'https',ip,port)
+            t = proxy_test(proxyConfig,'https',ip,port)
+            t.start()
+            t.join()
 
 def ip181_proxy():
     for item in range(1,11):
@@ -76,9 +78,13 @@ def ip181_proxy():
             proxyConfig = ip+":"+port
             if "," in protocol:
                 for item in protocol.split(','):
-                    proxy_test(proxyConfig,item,ip,port)
+                    t = proxy_test(proxyConfig,protocol,ip,port)
+                    t.start()
+                    t.join()
             else:
-                proxy_test(proxyConfig,protocol,ip,port)
+                t = proxy_test(proxyConfig,protocol,ip,port)
+                t.start()
+                t.join()
 
 
 def ipcn_proxy():
@@ -89,23 +95,60 @@ def ipcn_proxy():
             ip = proxyConfig.split(':')[0]
             port = proxyConfig.split(':')[1]
             for protocol in ['https','http']:
-                proxy_test(proxyConfig,protocol,ip,port)
+                t = proxy_test(proxyConfig,protocol,ip,port)
+                t.start()
+                t.join()
 
 
-def proxy_test(proxyConfig,protocol,ip,port):
-    try:
-        if not re.search(r"\d+\.\d+\.\d+\.\d+",ip):
-            print "[x]Proxy requests error."
-            return
-        speed_test = urllib.urlopen('http://www.baidu.com', proxies={protocol.lower():proxyConfig}).read()
-        if "baidu" not in ''.join(speed_test):
-            print "[x]Proxy requests error."
-            return
-        print '[+]ip:'+proxyConfig+" for "+protocol
-        ipfile.writelines(protocol.lower()+' '+ip+' '+port+'\n')
-    except Exception,e:
-        print e
+#国外的代理
+def ljf_proxy():
+    proxy_urls = [
+            #OPENSSL需要高版本
+            #"https://www.us-proxy.org/",
+            "http://free-proxy-list.net/uk-proxy.html",
+            "http://www.sslproxies.org/"
+            ]
+    for proxy_url in proxy_urls:
+        resp = requests.get(proxy_url ,headers = headers).content
+        soup = BeautifulSoup(resp)
+        tbody = soup.find('tbody')
+        trs = tbody.find_all('tr')
+        for tr in trs:
+            tds = tr.find_all('td')
+            ip = tds[0].text
+            port = tds[1].text
+            protocol = tds[3].text.lower()
+            proxyConfig = ip+":"+port
+            for protocol in ['https','http']:
+                t = proxy_test(proxyConfig,protocol,ip,port)
+                t.start()
+                t.join()
+
+
+class proxy_test(threading.Thread):
+    def __init__(self, proxyConfig,protocol,ip,port):  
+        threading.Thread.__init__(self)  
+        self.proxyConfig = proxyConfig
+        self.protocol = protocol
+        self.ip = ip
+        self.port = port
+   
+    def run(self):
+        try:
+            if not re.search(r"\d+\.\d+\.\d+\.\d+",self.ip):
+                print "[x]Proxy requests error."
+                return
+            speed_test = urllib.urlopen('http://www.baidu.com', proxies={self.protocol.lower():self.proxyConfig}).read()
+            if "baidu" not in ''.join(speed_test):
+                print "[x]Proxy requests error."
+                return
+            print '[+]ip:'+self.proxyConfig+" for "+self.protocol
+            ipfile.writelines(self.protocol.lower()+' '+self.ip+' '+self.port+'\n')
+        except Exception,e:
+            print e
         return
+
+
 
 
 if __name__ == "__main__":
@@ -114,5 +157,6 @@ if __name__ == "__main__":
     #xici_proxy()
     #cz88_proxy()
     #ip181_proxy()
-    #ipcn_proxy()
+    ipcn_proxy()
+    #ljf_proxy()
     ipfile.close()
